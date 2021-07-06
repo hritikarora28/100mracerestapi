2ec054c9be190c3560bde5d0101fa388f5e1aa7a
Hritik Arora <hritikarora875@gmail.com>
2021-07-06 12:58:54 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
-const express = require('express')
-require("../src/db/conn")
-const MensRanking = require("../src/models/mens")
+const express = require('express');
+require('../src/db/conn');
+const MensRanking = require('../src/models/mens');
 
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
+app.use(express.json())
 
-app.get("/",async(req,res)=>{
-    res.send("Hey there")
-})
+app.post('/mens', async (req, res) => {
+	try {
+		const addinMensRecords = new MensRanking(req.body);
+        console.log(req.body)
+	    const insertMens =	await addinMensRecords.save();
+        res.send(insertMens)
+	} catch (e) {
+		res.send(e);
+	}
+});
+app.get('/', async (req, res) => {
+	res.send('Hey there');
+});
 
-
-app.listen(port,()=>{
-    console.log(`your reques is send to the port ${port}`)
-})
+app.listen(port, () => {
+	console.log(`your reques is send to the port ${port}`);
+});
