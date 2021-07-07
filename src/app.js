f10cf2da72aa59917a6e2160525c7c68576ac860
Hritik Arora <hritikarora875@gmail.com>
2021-07-07 13:47:02 +0530

    3rd commit

--- src/app.js
+++ src/app.js
@@ -4,16 +4,33 @@ const MensRanking = require('../src/models/mens');
 
 const app = express();
 const port = process.env.PORT || 4000;
-app.use(express.json())
+app.use(express.json());
 
 app.post('/mens', async (req, res) => {
 	try {
 		const addinMensRecords = new MensRanking(req.body);
-        console.log(req.body)
-	    const insertMens =	await addinMensRecords.save();
-        res.send(insertMens)
+		console.log(req.body);
+		const insertMens = await addinMensRecords.save();
+		res.status(201).send(insertMens);
 	} catch (e) {
-		res.send(e);
+		res.status(400).send(e);
+	}
+});
+app.get('/mens/:id', async (req, res) => {
+	try {
+		const _id = req.params.id;
+		const getMen = await MensRanking.findById(_id);
+		res.status(200).send(getMen);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+app.get('/mens', async (req, res) => {
+	try {
+		const getMens = await MensRanking.find({});
+		res.status(200).send(getMens);
+	} catch (e) {
+		res.status(400).send(e);
 	}
 });
 app.get('/', async (req, res) => {
